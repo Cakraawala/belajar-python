465044617eb87b1b40951f8d2a7448592ac36d39
CAKRA <kyuackerman30@gmail.com>
2022-06-07 20:49:18 +0700

    dasar python 1 : comparasi data boolean

--- 4.komparasi.py
+++ 4.komparasi.py
@@ -1,51 +1,42 @@
-##operasi komparasi
+#operasi komparasi
 
-#boolean
+# boolean
 # >, <, >=, <=, ==, !=, is , is not
 
-# a = 4
-# b = 3
-# #lebih besar
-# hasil = a > b
-# print(a, '>', b, hasil)
+a = 4
+b = 3
+#lebih besar
+hasil = a > b
+print(a, '>', b, hasil)
 
-# #lebih kecil
-# hasil = a < b
-# print(a, '<', b, hasil)
+#lebih kecil
+hasil = a < b
+print(a, '<', b, hasil)
 
-# #lebih besar sama dengan
-# hasil = a >= b
-# print(a, '>=', b, hasil)
+#lebih besar sama dengan
+hasil = a >= b
+print(a, '>=', b, hasil)
 
-# #lebih kecil sama dengan
-# hasil = a <= b
-# print(a, '<=', b, hasil)
+#lebih kecil sama dengan
+hasil = a <= b
+print(a, '<=', b, hasil)
 
-# #sama dengan
-# hasil = a == b
-# print(a, '==', b, hasil)
+#sama dengan
+hasil = a == b
+print(a, '==', b, hasil)
 
-# #tidak sama dengan
-# hasil = a != b
-# print(a, '!=', b, hasil)
+#tidak sama dengan
+hasil = a != b
+print(a, '!=', b, hasil)
 
-# #x adalah y
-# hasil = a is b
-# print(a, 'is', b, hasil)
+#x adalah y
+hasil = a is b
+print(a, 'is', b, hasil)
 
-# #x tidak adalah y
-# hasil = a is not b
-# print(a, 'is not', b, hasil)
+#x tidak adalah y
+hasil = a is not b
+print(a, 'is not', b, hasil)
 
-# hasil = a is b
-# print(a, 'is' , b,  hasil)
+hasil = a is b
+print(a, 'is' , b,  hasil)
 
-
-##latihan logika komparasi
-# +++++3 --------------10 +++++++
-inputUser = float(input("masukan angka yang bernilai\n kurang dari 3\n atau\n lebih dari besar dari 10\n:"))
-
-# ++++++ 3 ---------------------
-# memeriksa angka kurang dari 3
-isKurangDari = (inputUser < 3)
-print(isKurangDari) 
