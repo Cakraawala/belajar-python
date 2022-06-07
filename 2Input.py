dde0f1c1c1b1d5f4642b5d3286987366851fc43c
CAKRA <kyuackerman30@gmail.com>
2022-06-07 20:43:31 +0700

    dasar python 1 :input data

---
+++ 2Input.py
@@ -0,0 +1,18 @@
+### Input user ------------------------------- ##
+#data input = string
+nama = input("masukkan nama : ")
+kelas = input("Kelas : ")
+jurusan = input("Jurusan : ")
+print("nama = ", nama, type(nama))
+print("kelas = ", kelas, jurusan, type(kelas))
+
+#data int
+
+# data2 = int(input("masukan angka :"))
+data2 = float(input("masukan angka :"))
+print("data = " , data2, type(data2))
+
+# data boolean
+#pakai int jika mau format angka
+biner = bool(int(input("masukan nilai boolean: ")))
+print("data = " , biner, type(biner))
