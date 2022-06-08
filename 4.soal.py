f21424b7dd4b543f91f0f5ecd396d1df71e36390
CAKRA <kyuackerman30@gmail.com>
2022-06-08 22:35:38 +0700

    dasar python 1 : latihan komparasi data boolean 2

--- 4.soal.py
+++ 4.soal.py
@@ -1,6 +1,6 @@
 #1 ----- 0 +++++++ 5 -------- 8 ++++++ 11 ------
 
-inputUser = float(input("masukan angka yang bernilai\n lebih dari sama dengan 0 sampai 5\n dan \n lebih dari sama dengan 8 sampai 11\n:"))
+inputUser = float(input("masukan angka yang bernilai\n lebih dari sama dengan 0 sampai 5\n dan \n lebih dari sama dengan 8 sampai 11 :  "))
 
 iskosonglima = (inputUser >= 0 <=5)
 print("Apakah",inputUser, "lebih dari 0 sampai 5 =",iskosonglima) 
@@ -14,7 +14,7 @@ print("\n")
 
 #2 +++++ 0 ------- 5 ++++++++ 8 ------ 11 ++++++
 
-inputUser = float(input("masukan angka yang bernilai\n kurang dari sama dengan 0\n lebih dari sama dengan 5 sampai 8\n dan lebih dari sama dengan 11 :\n"))
+inputUser = float(input("masukan angka yang bernilai\n kurang dari sama dengan 0\n lebih dari sama dengan 5 sampai 8\n dan lebih dari sama dengan 11 :  "))
 
 iskosong = (inputUser <= 0)
 print("Apakah",inputUser, "kurang dari 0 =",iskosong) 
